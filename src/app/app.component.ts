92e8e58099ef0a43055809987cd3524f27aec228
niyyatii19 <niyatigupta.998@gmail.com>
2024-01-04 23:21:34 +0530

    added bootstarp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'vratino_f';
+
+
 }
